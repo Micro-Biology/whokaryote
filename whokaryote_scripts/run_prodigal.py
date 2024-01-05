9f3554a876045561705da63dde22eb85fb336d0f
Jono Warren <34277641+Micro-Biology@users.noreply.github.com>
2024-01-05 11:50:51 +0000

    Update run_prodigal.py

--- whokaryote_scripts/run_prodigal.py
+++ whokaryote_scripts/run_prodigal.py
@@ -4,9 +4,9 @@ import subprocess
 import os
 
 
-def run_prodigal(contig_file, outdir):
+def run_prodigal(contig_file, outdir, threads):
     genes_output = os.path.join(outdir, "contigs_genes.gff")
     proteins_output = os.path.join(outdir, "contigs_proteins.faa")
-
+    print(f"    pprodigal using {threads} threads and splitting into 2000 sequences per chunk")
     subprocess.call(
-        ["prodigal", "-i", contig_file, "-o", genes_output, "-a", proteins_output, "-p", "meta", "-f", "gff", "-q"])
+        ["pprodigal", "-i", contig_file, "-o", genes_output, "-a", proteins_output, "-p", "meta", "-f", "gff", "-q", "-T", threads])
